f9fca8c3aceeaca13ba74ed9d58d37130acb208b
Christoph Paus <paus@mit.edu>
2016-01-26 16:33:57 -0500

    Adding dropdown menus.

--- web/app/views/admin/header.php
+++ web/app/views/admin/header.php
@@ -2,7 +2,7 @@
 
 include("app/views/header.php");
 
-if (isAdmin() || isMaster()) {
+if (isAdmin() && !isMaster()) {
   print '<div style="font-family: arial, verdana, sans-serif; font-size: 20px; color: darkred; background-color: pink" class="transbox">'."\n";
   print '&nbsp; Admins:'."\n";
   print '&nbsp; <a href="/admin"> <span class="fa fa-home">Home</span></a> &nbsp;'."\n";
@@ -14,26 +14,77 @@ if (isAdmin() || isMaster()) {
   print ' <a href="/assignments">all</a>'."\n";
   print ' <a href="/showActiveAssignments">active</a>'."\n";
   print '</div>'."\n";
-}
-else {
-  print ' You are no admin.<br>';
+
+  print '<div style="font-family: arial, verdana, sans-serif; font-size: 20px" class="transbox">'."\n";
+  print " <div id='cssmenu'>";
+  print " <ul>";
+  print "    <li class='active'><a href='/admin'><span class=\"fa fa-home\">Admin</span></a></li>";
+  print "    <li class='has-sub'><a href='#'><span>Courses</span></a>";
+  print "       <ul>";
+  print "          <li><a href='/courses'><span>all</span></a></li>";
+  print "       </ul>";
+  print "    </li>";
+  print "    <li class='has-sub'><a href='#'><span>Students</span></a>";
+  print "       <ul>";
+  print "          <li><a href='/students'><span>all</span></a></li>";
+  print "          <li><a href='/showActiveTas'><span>active</span></a></li>";
+  print "       </ul>";
+  print "    </li>";
+  print "    <li class='has-sub'><a href='#'><span>Assignments</span></a>";
+  print "       <ul>";
+  print "          <li><a href='/assignments'><span>all</span></a></li>";
+  print "          <li><a href='/showActiveAssignments'><span>active</span></a></li>";
+  print "          <li><a href='/showLastAssignments'><span>last</span></a></li>";
+  print "       </ul>";
+  print "    </li>";
+  print " </ul>";
+  print " </div>";
+  print "</div>"."\n";
 }
 
 if (isMaster()) {
-  print '<div style="font-family: arial, verdana, sans-serif; font-size: 20px; color: darkgreen; background-color: mintgreen" class="transbox">'."\n";
-  print '&nbsp; Master:'."\n";
-  print ' &nbsp; evaluations:';
-  print ' <a href="/evaluations">all</a>'."\n";
-  print ' <a href="/showActiveEvaluations">active</a>'."\n";
-  print ' <a href="/showEvaluators">evaluators</a>'."\n";
-  print ' <a href="/addEvaluation">evaluate</a>'."\n";
-  print ' &nbsp; misc:';
-  print ' <a href="/showPreferences">preferences</a>'."\n";
-  print ' <a href="/showAccessLists">access</a>'."\n";
-  print ' <a href="/showLastAssignments">lastA</a>'."\n";
-  print ' <a href="/copyLastAssignments">copyL</a>'."\n";
-  print ' <a href="/findTas">findTas</a>'."\n";
-  print ' <a href="/email">email</a>'."\n";
-  print '</div>'."\n";
+
+  print '<div style="font-family: arial, verdana, sans-serif; font-size: 20px" class="transbox">'."\n";
+  print " <div id='cssmenu'>";
+  print " <ul>";
+  print "    <li class='active'><a href='/admin'><span class=\"fa fa-home\">Master</span></a></li>";
+  print "    <li class='has-sub'><a href='#'><span>Courses</span></a>";
+  print "       <ul>";
+  print "          <li><a href='/courses'><span>all</span></a></li>";
+  print "       </ul>";
+  print "    </li>";
+  print "    <li class='has-sub'><a href='#'><span>Students</span></a>";
+  print "       <ul>";
+  print "          <li><a href='/students'><span>all</span></a></li>";
+  print "          <li><a href='/showActiveTas'><span>active</span></a></li>";
+  print "          <li><a href='/showPreferences'><span>preferences</span></a></li>";
+  print "       </ul>";
+  print "    </li>";
+  print "    <li class='has-sub'><a href='#'><span>Assignments</span></a>";
+  print "       <ul>";
+  print "          <li><a href='/assignments'><span>all</span></a></li>";
+  print "          <li><a href='/showActiveAssignments'><span>active</span></a></li>";
+  print "          <li><a href='/showLastAssignments'><span>last</span></a></li>";
+  print "          <li><a href='/copyLastAssignments'><span>copy-last</span></a></li>";
+  print "       </ul>";
+  print "    </li>";
+  print "    <li class='has-sub'><a href='#'><span>Evaluations</span></a>";
+  print "       <ul>";
+  print "          <li><a href='/evaluations'><span>all</span></a></li>";
+  print "          <li><a href='/showActiveEvaluations'><span>active</span></a></li>";
+  print "          <li><a href='/addEvaluation'><span>evaluate</span></a></li>";
+  print "          <li><a href='/showEvaluators'><span>evaluators</span></a></li>";
+  print "       </ul>";
+  print "    </li>";
+  print "    <li class='has-sub'><a href='#'><span>Misc</span></a>";
+  print "       <ul>";
+  print "          <li><a href='/showAccessLists'><span>access lists</span></a></li>";
+  print "          <li><a href='/findTas'><span>find Tas</span></a></li>";
+  print "          <li><a href='/email'><span>email</span></a></li>";
+  print "       </ul>";
+  print "    </li>";
+  print " </ul>";
+  print " </div>";
+  print "</div>"."\n";
 }
 ?>
