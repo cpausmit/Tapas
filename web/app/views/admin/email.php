9012844bfa37b3db03dd9230403dfe692300151e
Christoph Paus <paus@mit.edu>
2016-02-04 17:25:06 -0500

    Working on the email interface.

--- web/app/views/admin/email.php
+++ web/app/views/admin/email.php
@@ -10,20 +10,26 @@ $email = strtolower($_SERVER['SSL_CLIENT_S_DN_Email']);
 
 print '<article class="page">'."\n";
 print "<hr>\n";
-print "<h1>Composing an Email</h1>\n<h2> Christoph ($email)</h2>\n";
+print "<h1>Composing an Email</h1>\n<h2> From $email</h2>\n";
 print "<hr>\n";
 
 // loop through all assignment tables and find our candidate
 
 // Send
-$subject = 'EMPTY';
+$subject = 'EMPTY SUBJECT';
 $message = "\n\nCheers, Christoph";
 $headers = "From: Christoph Paus <paus@mit.edu>\nReply-To: <paus@mit.edu>";
 print " THE MESSAGE: $message<br>\nTO: $email<br>";
 
 // start the form
 print '<p>';
-print "<form action=\"/sendEmail?to=TAs\" method=\"post\">\n";
+print "<form action=\"/sendEmail\" method=\"post\">\n";
+print '  <select class="Recipients" name="Recipients">'."\n";
+print '  <option value="">Select recipients</option>'."\n";
+print "  <option value=\"Myself\"> Myself </option>";
+print "  <option value=\"TAs\"> TAs </option>";
+print "  <option value=\"Teachers\"> Teachers </option>";
+print '  </select>'."\n";
 print "Write a message to the active TAs below.\n";
 print '<textarea style="font-family: arial; font-size: 20px; color: black;" name="Subject" rows=1 cols=40>'.$subject.'</textarea>';
 print '<textarea style="font-family: arial; font-size: 20px; color: black;" name="Message" rows=8 cols=80>'.$message.'</textarea>';
--- web/app/views/admin/sendEmail.php
+++ web/app/views/admin/sendEmail.php
@@ -8,19 +8,24 @@ if (! isMaster()) {
 // command line arguments
 $email = strtolower($_SERVER['SSL_CLIENT_S_DN_Email']);
 
-if (isset($_GET['to']))
-  $targetString = $_GET['to'];
+//if (isset($_GET['to']))
+//  $targetString = $_GET['to'];
+if (isset($_POST['Recipients']))
+  $targetString = $_POST['Recipients'];
 if (isset($_POST['Subject']))
   $subject = $_POST['Subject'];
 if (isset($_POST['Message']))
   $message = $_POST['Message'];
 
-$cc       = "Cc: paus@mit.edu,nergis@mit.edu,cmodica@mit.edu";
-$headers  = "From: Christoph Paus <paus@mit.edu>\nReply-To: <paus@mit.edu>\n$cc";
+$headers  = "From: Christoph Paus <paus@mit.edu>\nReply-To: <paus@mit.edu>\n";
+
+//$cc       = "Cc: paus@mit.edu,nergis@mit.edu,cmodica@mit.edu";
+//$headers  = "From: Christoph Paus <paus@mit.edu>\nReply-To: <paus@mit.edu>\n$cc";
 
 // retrieve emails of recipients
 include("app/models/Dbc.php");
 $db = Dbc::getReader();
+
 if ($targetString == "TAs") {
   // find active Ta table
   $activeTables = new ActiveTables($db);
@@ -35,25 +40,48 @@ if ($targetString == "TAs") {
       $list = "$list,$row[0]";
   }
 } 
+else if ($targetString == "Teachers") {
+
+  $link = getLink();
+ 
+  // do the query
+  $rows = readTeacherTable($link);
+  $list = "";
+  foreach ($rows as $key => $row) {
+    if ($list == "")
+      $list = "$row";
+    else
+      $list = "$list,$row";
+  }
+} 
+else if ($targetString == "Myself") {
+
+  $list = "$email";
+} 
 
 print '<article class="page">'."\n";
 print "<hr>\n";
 print "<h1>Sending the Email</h1>\n";
 print "<h2> Christoph ($email)</h2>\n";
 print "<hr>\n";
-print " TO:<br>      $list<br>\n";
-print " SUBJECT:<br> $subject<br>\n";
+print " IdString:    $targetString<br><br>\n";
+print " TO:          $list<br>\n";
+print " SUBJECT:     $subject<br>\n";
 print " MESSAGE:<br> $message<br>\n";
 print " HEADERS:<br> $headers<br>\n";
 
 // Send
-print "<p>Mail was NOT sent. Feature disabled for now!</p>";
 
-//if (mail($list,$subject,$message,$headers))
-//  print "<p>Mail accepted for delivery (does not guarantee it reaches its destination).</p>";
-//else
-//  print "<p>Mail was NOT accepted for delivery.</p>";
-
+if (True) {
+  print "<p>Mail was NOT sent. Feature disabled for now!</p>";
+}
+else {
+ if (mail($list,$subject,$message,$headers))
+   print "<p>Mail accepted for delivery (does not guarantee delivery though).</p>";
+ else
+   print "<p>Mail was NOT accepted for delivery.</p>";
+}
+   
 print "<hr>\n";
 print "</article>\n";
 
